f45a0878856f6c9173b50a1bbd3e520cb69e0385
geon lee <geon.lee@navercorp.com>
2021-01-06 02:25:12 +0900

    🔧 ad margin style

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,6 +4,10 @@ import { faBolt } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import styled from 'styles/themed-components'
 
+const Advertisement = styled.div`
+  margin: 1rem 0;
+`
+
 const PoweredBy = styled.div``
 
 function Footer(): ReactElement {
@@ -13,14 +17,16 @@ function Footer(): ReactElement {
         margin: '3rem 0 1.5rem',
       }}>
       <div>
-        <ins
-          className="adsbygoogle"
-          style={{ display: 'block' }}
-          data-ad-client="ca-pub-9994255438328666"
-          data-ad-slot="8074364163"
-          data-ad-format="auto"
-          data-full-width-responsive="true"
-        />
+        <Advertisement>
+          <ins
+            className="adsbygoogle"
+            style={{ display: 'block' }}
+            data-ad-client="ca-pub-9994255438328666"
+            data-ad-slot="8074364163"
+            data-ad-format="auto"
+            data-full-width-responsive="true"
+          />
+        </Advertisement>
         <PoweredBy>
           <FontAwesomeIcon icon={faBolt} style={{ marginRight: '0.5rem' }} />
           Powered by{' '}
