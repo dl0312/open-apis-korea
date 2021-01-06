6a246bc539f5b36a0e862635f2300efe717e4454
geon lee <geon.lee@navercorp.com>
2021-01-07 01:51:51 +0900

    ✨ align

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,6 +4,12 @@ import { faBolt } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import styled from 'styles/themed-components'
 
+const Container = styled.footer`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin: 3rem 0 1.5rem;
+`
 const Advertisement = styled.div`
   display: flex;
   flex-wrap: wrap;
@@ -15,44 +21,39 @@ const PoweredBy = styled.div``
 
 function Footer(): ReactElement {
   return (
-    <footer
-      style={{
-        margin: '3rem 0 1.5rem',
-      }}>
-      <div>
-        <Advertisement>
-          <ins
-            className="kakao_ad_area"
-            style={{ display: 'none' }}
-            data-ad-unit="DAN-TLKSG6Khw4lDAQVy"
-            data-ad-width="300"
-            data-ad-height="250"
-          />
-          <ins
-            className="kakao_ad_area"
-            style={{ display: 'none' }}
-            data-ad-unit="DAN-P6gv4cKjhpS3GPJH"
-            data-ad-width="300"
-            data-ad-height="250"
-          />
-          <ins
-            className="adsbygoogle"
-            style={{ display: 'block' }}
-            data-ad-client="ca-pub-9994255438328666"
-            data-ad-slot="8074364163"
-            data-ad-format="auto"
-            data-full-width-responsive="true"
-          />
-        </Advertisement>
-        <PoweredBy>
-          <FontAwesomeIcon icon={faBolt} style={{ marginRight: '0.5rem' }} />
-          Powered by{' '}
-          <a href="https://api.publicapis.org/" target="_blank">
-            Public API for Public APIs
-          </a>
-        </PoweredBy>
-      </div>
-    </footer>
+    <Container>
+      <Advertisement>
+        <ins
+          className="kakao_ad_area"
+          style={{ display: 'none' }}
+          data-ad-unit="DAN-TLKSG6Khw4lDAQVy"
+          data-ad-width="300"
+          data-ad-height="250"
+        />
+        <ins
+          className="kakao_ad_area"
+          style={{ display: 'none' }}
+          data-ad-unit="DAN-P6gv4cKjhpS3GPJH"
+          data-ad-width="300"
+          data-ad-height="250"
+        />
+        <ins
+          className="adsbygoogle"
+          style={{ display: 'block' }}
+          data-ad-client="ca-pub-9994255438328666"
+          data-ad-slot="8074364163"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        />
+      </Advertisement>
+      <PoweredBy>
+        <FontAwesomeIcon icon={faBolt} style={{ marginRight: '0.5rem' }} />
+        Powered by{' '}
+        <a href="https://api.publicapis.org/" target="_blank">
+          Public API for Public APIs
+        </a>
+      </PoweredBy>
+    </Container>
   )
 }
 
