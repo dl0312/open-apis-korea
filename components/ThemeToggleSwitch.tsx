0a24fb1cd77d8eb714c506f2e73ead6362a6a5d3
geon lee <geon.lee@navercorp.com>
2021-01-06 17:09:14 +0900

    🎨 remove style

--- components/ThemeToggleSwitch.tsx
+++ components/ThemeToggleSwitch.tsx
@@ -1,20 +1,12 @@
 import React, { ReactElement } from 'react'
 
 import { Switch } from 'antd'
-import styled from 'styled-components'
 import useDarkMode from 'use-dark-mode'
 
-const Container = styled.div`
-  margin-bottom: 0.5rem;
-`
 export default function ThemeToggleSwitch(): ReactElement {
   const darkMode = useDarkMode(false)
   const handleOnClickToggle = () => {
     darkMode.toggle()
   }
-  return (
-    <Container>
-      <Switch checkedChildren="🌞" unCheckedChildren="🌚" checked={!darkMode.value} onClick={handleOnClickToggle} />
-    </Container>
-  )
+  return <Switch checkedChildren="🌞" unCheckedChildren="🌚" checked={!darkMode.value} onClick={handleOnClickToggle} />
 }
